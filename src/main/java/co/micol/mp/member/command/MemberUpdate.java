f1320e921d4136ce08fe8946c82b13af17d93082
admin <admin@YD02-13>
2023-06-19 12:03:35 +0900

    0619 오전 커밋

--- src/main/java/co/micol/mp/member/command/MemberUpdate.java
+++ src/main/java/co/micol/mp/member/command/MemberUpdate.java
@@ -2,6 +2,7 @@ package co.micol.mp.member.command;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import co.micol.mp.common.Command;
 import co.micol.mp.member.service.MemberService;
@@ -13,6 +14,7 @@ public class MemberUpdate implements Command {
 	@Override
 	public String exec(HttpServletRequest request, HttpServletResponse response) {
 		// 회원정보수정 실행
+		
 		MemberService ms = new MemberServiceImpl();
 		MemberVO vo = new MemberVO();
 		vo.setMemberId(request.getParameter("memberId"));
@@ -20,6 +22,11 @@ public class MemberUpdate implements Command {
 		vo.setMemberName(request.getParameter("memberName"));
 		vo.setMemberTel(request.getParameter("memberTel"));
 		ms.memberUpdate(vo);
+		
+		//세션 삭제
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
 		
 		return "main/main";
 	}
